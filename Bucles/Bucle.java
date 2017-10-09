5285010c927b81e30d7adb5f8ab0e5ee1d12e3fe
Pedro Molina <pedromolesp@gmail.com>
2017-10-09 13:22:25 +0200

    Subir Java

---
+++ Bucles/Bucle.java
@@ -0,0 +1,5 @@
+public class Bucle{
+public static void main(String [] args){
+
+System.out.println("Comentario")}
+}
---
+++ Datos/tiposDatos.java
@@ -0,0 +1,5 @@
+public class tiposDatos{
+public static void main(String [] args){
+
+System.out.println("Comentario")}
+}
